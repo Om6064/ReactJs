3d7f8110d98b64e020d526106d72ddf1ae1b80c4
om6064 <aryanpatel.lihodiya@gmail.com>
2025-05-28 11:32:16 +0530

    added all feature in todo list with ui

--- todo-list/src/components/Table.jsx
+++ todo-list/src/components/Table.jsx
@@ -1,17 +1,17 @@
-const Table = ({ id, name, complete, onComplete }) => {
+const Table = (props) => {
   return (
     <li className="bg-gray-100 rounded-lg flex justify-between items-center px-4 py-3 shadow-sm">
       <span
-        className={`text-base font-medium ${complete ? "line-through text-green-600" : "text-gray-800"}`}
+        className={`text-base font-medium ${props.complete ? "line-through text-green-600" : "text-gray-800"}`}
       >
-        {name}
+        {props.name}
       </span>
 
-      {complete ? (
+      {props.complete ? (
         ""
       ) : (
         <button
-          onClick={() => onComplete(id)}
+          onClick={() => props.onComplete(props.id)}
           className="w-8 h-8 bg-green-500 text-white rounded-full flex items-center justify-center hover:bg-green-600 shadow-md transition"
         >
           ✔
