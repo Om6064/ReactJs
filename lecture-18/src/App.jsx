10b90440b0a6bfae326a16731348c70ee11548f7
om6064 <aryanpatel.lihodiya@gmail.com>
2025-07-23 16:56:43 +0530

    added json server crud

--- lecture-18/src/App.jsx
+++ lecture-18/src/App.jsx
@@ -1,27 +1,73 @@
+import axios from "axios"
 import { useEffect, useState } from "react"
 
 const App = () => {
 
-    const [user,setData] = useState([])
+    const [input, setInput] = useState({
+        name: "", age: ""
+    })
+
+    const [user, setData] = useState([])
+    const [isedit, setisedit] = useState(false)
+    const fetchData = async () => {
+        let { data } = await axios.get("http://localhost:8175/users")
+        setData(data)
+    }
 
     useEffect(() => {
-        const fetchData = async () => {
-            let URL = "http://localhost:8175/users"
-            let res = await fetch(URL)
-            let data = await res.json()
-            console.log(data);
-            
-            setData(data)
-        }
         fetchData()
     }, [])
+
+    const handleAdd = async (id) => {
+        if (isedit == false) {
+            await axios.post("http://localhost:8175/users", {
+                ...input, id: Date.now().toString()
+            })
+            fetchData();
+            setInput({ name: '', age: "" })
+        }else{
+            await axios.put(`http://localhost:8175/users/${id}`,input)
+            fetchData()
+            setInput({ name: '', age: "" })
+        }
+    }
+
+    const handleDelete = async (id) => {
+        await axios.delete(`http://localhost:8175/users/${id}`)
+        fetchData()
+    }
+
+    const handleEdit = async (id) => {
+        let { data } = await axios.get(`http://localhost:8175/users/${id}`)
+        setInput(data)
+        setisedit(true)
+    }
     return (
         <div>
+            <input type="text" value={input.name} id="name" onChange={(e) => {
+                setInput({ ...input, [e.target.id]: e.target.value })
+            }} />
+            <br />
+            <br />
+            <input type="number" value={input.age} id="age" onChange={(e) => {
+                setInput({ ...input, [e.target.id]: e.target.value })
+            }} />
+            <br />
+            <br />
+            <button onClick={() => {
+                handleAdd(input.id)
+            }}>Add User</button>
             {user.map((user) => {
-                return <>
+                return <div key={user.id}>
                     <h1>{user.name}</h1>
                     <h1>{user.age}</h1>
-                </>
+                    <button onClick={() => {
+                        handleDelete(user.id)
+                    }}>X</button>
+                    <button onClick={() => {
+                        handleEdit(user.id)
+                    }}>Edit</button>
+                </div>
             })}
         </div>
     )
